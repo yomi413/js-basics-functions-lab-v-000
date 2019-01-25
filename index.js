9c1ddd746247f83ec840590d52d3c370a7b22d17
Yomaira Escano <yjescano@gmail.com>
2019-01-25 17:49:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,43 +1,40 @@
 // Code your solution in this file!
-function distanceFromHqInBlocks(pickupLocation) {
-  let hq = 42;
-  let distance;
+function distanceFromHqInBlocks(blocks) {
+  const hq = 42;
 
-  if (pickupLocation === hq + 1) {
-    distance = 1;
-  } else if (pickupLocation === hq - 8 || pickupLocation === hq + 8) {
-    distance = 8;
+  if (blocks > hq) {
+    return blocks - hq;
+  } else if (blocks < hq) {
+    return hq - blocks;
   }
-  return distance
 }
 
-function distanceFromHqInFeet(passenger) {
-  return distanceFromHqInBlocks(passenger) * 264;
+function distanceFromHqInFeet(blocks) {
+  const blockLength = 264;
+
+  return distanceFromHqInBlocks(blocks) * blockLength;
 }
 
-function distanceTravelledInFeet(start, destination) {
-  let distance;
-
-  if (start < destination) {
-    distance = (destination - start) * 264;
-  } else if (start > destination) {
-    distance = (start - destination) * 264;
+function distanceTravelledInFeet(startingBlock, endingBlock) {
+  const blockLength = 264;
+  
+  if (endingBlock > startingBlock) {
+    return (endingBlock - startingBlock) * blockLength;
+  } else if (endingBlock < startingBlock) {
+    return (startingBlock - endingBlock) * blockLength;
   }
-  return distance
 }
 
 function calculatesFarePrice(start, destination) {
-  let fare;
-  let distance = distanceTravelledInFeet(start, destination)
+  let distance = distanceTravelledInFeet(start, destination);
 
-  if (distance < 400) {
-    fare = 0;
+  if (distance <= 400) {
+    return (distance) * 0;
   } else if (distance > 400 && distance < 2000) {
-    fare = (distance - 400) * 0.02;
+    return (distance - 400) * .02;
   } else if (distance > 2000 && distance < 2500) {
-    fare = 25;
-  } else if (distance > 2500) {
-    fare = 'cannot travel that far';
+    return 25;
+  } else {
+    return 'cannot travel that far';
   }
-  return fare
 }
